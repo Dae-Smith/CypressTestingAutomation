255f3f19585f39347cfe7fa47c9b30991247bce7
DaeQuan Smith <daequantsmith@gmail.com>
2023-05-31 20:37:02 -0500

    2nd commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+//cypress - Test files are called Specs
+
+describe('My First Test Suite', function(){
+    it('My first test case', function(){
+    
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    })
+})
